33aaf3022ddd48a1c9299d81619128c43b405154
Nikolai Kucksdorf <nikolai.kucksdorf@live.de>
2024-06-17 20:18:54 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 const button_bank = '<div id="rolltype-buttons" class="rolltype-container">' +
-                        '<button class="rolltype-button publicroll" name="publicroll"><i class="fa-solid fa-dice-d20 fa-lg"></i></button>' +
-                        '<button class="rolltype-button gmroll" name="gmroll"><i class="fa-solid fa-user-secret fa-lg"></i></button>' +
-                        '<button class="rolltype-button blindroll" name="blindroll"><i class="fa-solid fa-eye-low-vision fa-lg"></i></button>' +
-                        '<button class="rolltype-button selfroll" name="selfroll"><i class="fa-solid fa-ghost fa-lg"></i></button>' +
+                        '<button class="rolltype-button publicroll" name="publicroll" data-tooltip="Roll publicly"><i class="fa-solid fa-dice-d20 fa-lg"></i></button>' +
+                        '<button class="rolltype-button gmroll" name="gmroll" data-tooltip="Roll to the GM"><i class="fa-solid fa-user-secret fa-lg"></i></button>' +
+                        '<button class="rolltype-button blindroll" name="blindroll" data-tooltip="Roll blindly"><i class="fa-solid fa-eye-low-vision fa-lg"></i></button>' +
+                        '<button class="rolltype-button selfroll" name="selfroll" data-tooltip="Roll to yourself"><i class="fa-solid fa-ghost fa-lg"></i></button>' +
                     '</div>';
 
 
-Hooks.once('ready', function(){
+Hooks.on('renderSidebarTab', function(){
     hideSelect();
     addButtons();
     bindEventListeners();
@@ -14,6 +14,7 @@ Hooks.once('ready', function(){
 
 function hideSelect() {
     $('#chat-controls select').hide();
+    $('#rolltype-buttons').remove();
 }
 
 function addButtons() {
@@ -24,9 +25,10 @@ function addButtons() {
 
 function switchRollType(type) {
     $('#chat-controls select option:selected').removeAttr('selected');
-    $('#chat-controls select option[value="' + type + '"]').addAttr('selected');
+    $('#chat-controls select option[value="' + type + '"]').attr('selected');
     $('#rolltype-buttons .rolltype-button').removeClass('active');
     $('#rolltype-buttons .rolltype-button.'+ type).addClass('active');
+    localStorage.setItem('core.rollMode', type);
 }
 
 function bindEventListeners() {
